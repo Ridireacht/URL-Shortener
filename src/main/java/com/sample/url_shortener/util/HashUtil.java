a0457ef24e93df35db1cc094e3ec4ccd8a09cfd4
Venetian Doge <elvagyodasz@gmail.com>
2024-07-30 17:15:28 +0300

    Complete a Base62 hash util

--- src/main/java/com/sample/url_shortener/util/HashUtil.java
+++ src/main/java/com/sample/url_shortener/util/HashUtil.java
@@ -1,8 +1,45 @@
 package com.sample.url_shortener.util;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashUtil {
 
+    private static final String BASE62_ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+
     public static String generateHashForUrl(String url) {
-        return null;
+        // Generate SHA-256 hash first
+        byte[] hashBytes = toSHA256(url);
+
+        // Turn it to Base62 then
+        return toBase62(hashBytes);
+    }
+
+    private static byte[] toSHA256(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            return digest.digest(input.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Failed to generate SHA-256 hash", e);
+        }
+    }
+
+    private static String toBase62(byte[] bytes) {
+        // Turn byte array to a number
+        BigInteger number = new BigInteger(1, bytes);
+
+        // Generate Base62 hash
+        StringBuilder base62 = new StringBuilder();
+        while (number.compareTo(BigInteger.ZERO) > 0) {
+            BigInteger[] divMod = number.divideAndRemainder(BigInteger.valueOf(BASE62_ALPHABET.length()));
+            number = divMod[0];
+            int index = divMod[1].intValue();
+            base62.append(BASE62_ALPHABET.charAt(index));
+        }
+
+        // Reverse a hash so its symbols' order is correct
+        return base62.reverse().toString();
     }
 }
