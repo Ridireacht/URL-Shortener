db917613e78c85d5b27fe4afb51f397d65f1f65d
Venetian Doge <elvagyodasz@gmail.com>
2024-08-02 13:07:04 +0300

    Externalize app domain name to Docker environment var

--- src/main/java/com/sample/url_shortener/controller/UrlMappingController.java
+++ src/main/java/com/sample/url_shortener/controller/UrlMappingController.java
@@ -3,6 +3,7 @@ package com.sample.url_shortener.controller;
 import com.sample.url_shortener.dto.SaveRequestDTO;
 import com.sample.url_shortener.service.UrlMappingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,9 @@ public class UrlMappingController {
 
     private final UrlMappingService urlMappingService;
 
+    @Value("${APP_DOMAIN}")
+    private String appDomain;
+
 
     @PostMapping("/")
     public ResponseEntity<String> saveUrl(@RequestBody SaveRequestDTO saveRequestDTO) {
@@ -28,7 +32,7 @@ public class UrlMappingController {
         // Remove all unnecessary '/' at the end
         String url = saveRequestDTO.getUrl().replaceAll("/+$", "");
 
-        return ResponseEntity.ok().body("localhost:8080/" + urlMappingService.saveUrl(url));
+        return ResponseEntity.ok().body(appDomain + "/" + urlMappingService.saveUrl(url));
     }
 
     private static boolean isURL(String input) {
