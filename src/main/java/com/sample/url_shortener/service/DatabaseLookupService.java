a85661b4c9b209fad48284b864bbfa6afddeee27
Venetian Doge <elvagyodasz@gmail.com>
2024-10-05 14:18:56 +0300

    Remove old project structure

--- src/main/java/com/sample/url_shortener/service/DatabaseLookupService.java
+++
@@ -1,8 +0,0 @@
-package com.sample.url_shortener.service;
-
-public interface DatabaseLookupService {
-
-    String findUrlByHash(String hash);
-
-    String findHashByUrl(String url);
-}
--- src/main/java/com/sample/url_shortener/service/UrlMappingService.java
+++
@@ -1,6 +0,0 @@
-package com.sample.url_shortener.service;
-
-public interface UrlMappingService {
-
-    String saveUrl(String url);
-}
