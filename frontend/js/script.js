30da1264987cf774581cb87adedf5332453cb40a
Venetian Doge <elvagyodasz@gmail.com>
2024-10-06 13:42:40 +0300

    Set up proxying in Nginx

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        fetch('http://localhost:8080/api/', {
+        fetch('/api', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
