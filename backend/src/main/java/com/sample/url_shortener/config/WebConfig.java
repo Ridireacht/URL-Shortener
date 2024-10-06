fd2bfa7f7d26da11a158dba8c0b2fca1e8cebc19
Venetian Doge <elvagyodasz@gmail.com>
2024-10-06 19:16:31 +0300

    Remove unsupported HTTP methods from CORS

--- backend/src/main/java/com/sample/url_shortener/config/WebConfig.java
+++ backend/src/main/java/com/sample/url_shortener/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(appDomain)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST")
                 .allowCredentials(true);
     }
 }
