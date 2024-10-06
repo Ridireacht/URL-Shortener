2c38bc0be8e2838c7d6b90e4f82e126c98b1dcfa
Venetian Doge <elvagyodasz@gmail.com>
2024-10-06 18:27:43 +0300

    Change domain name to a not-backend one, use it in CORS

--- backend/src/main/java/com/sample/url_shortener/config/WebConfig.java
+++ backend/src/main/java/com/sample/url_shortener/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.sample.url_shortener.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${APP_DOMAIN}")
+    private String appDomain;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost")
+                .allowedOrigins(appDomain)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
